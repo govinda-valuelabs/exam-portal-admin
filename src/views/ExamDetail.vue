e3364b0b3e58a5e986a7a035e8a7df122e3db20e
Govinda Mandal <govinda.mandal@valuelabs.com>
2024-01-08 20:04:43 +0530

    Added feedbacks, attachments in the survey details

--- src/views/ExamDetail.vue
+++ src/views/ExamDetail.vue
@@ -11,10 +11,11 @@ export default {
     return {
       exam: null,
       loading: false,
+      answers: [],
       questions: [],
-      totalRead: 0,
+      feedbacks: {},
+      attachments: {},
       totalAttempt: 0,
-      totalCorrect: 0
     };
   },
   mounted() {
@@ -23,46 +24,36 @@ export default {
   methods: {
     calculateQuestions() {
       let totalAttempt = 0;
-      let totalRead = 0;
-      let totalCorrect = 0;
-      this.exam.questions.forEach((q) => {
-        totalRead ++;
-        if (q.status == 'attempted') {
-          totalAttempt ++;
-        }
-
-        const question = this.questions.find((q1) => q.questionId == q1._id);
-
-        if (question.answer == q.optionId) {
-          totalCorrect ++;
+      this.answers.forEach((a) => {
+        if (a.status == 'attempted') {
+          totalAttempt++;
         }
       });
-      this.totalRead = totalRead;
       this.totalAttempt = totalAttempt;
-      this.totalCorrect = totalCorrect;
     },
-    getStudentAnswer(qst) {
-      let question = isProxy(qst) ? toRaw(qst) : qst;
-      this.exam = toRaw(this.exam);
-      let examQuestion = this.exam?.questions.find(
-        (q) => q.questionId == question._id
-      );
-
-      if (!examQuestion || examQuestion.status != 'attempted') {
-        return null;
+    getStudentAnswer(questionId) {
+      let ans = '';
+      const answer = this.answers.find((a) => a.question == questionId);
+      if (answer) {
+        if (answer.answer) {
+          return answer.answer;
+        }
+        answer.option.forEach((o) => {
+          ans += o.value + ', '
+        });
       }
 
-      const options = isProxy(question.options) ? toRaw(question.options) : question.options;
+      if (ans) {
+        ans = ans.slice(0, -2);
+      }
 
-      const selectedOption = options.find((o) => o._id == examQuestion.optionId);
-      return selectedOption;
+      return ans;
     },
     async getQuestions() {
       try {
         const result = await axios.get('http://localhost:8080/question/exam/' + this.exam.category);
         if (result.status == 200) {
           this.questions = result.data;
-          this.calculateQuestions();
         }
       } catch (error) {
         console.log('error q ', error.message);
@@ -74,22 +65,65 @@ export default {
         if (result.status == 200) {
           this.exam = result.data;
           this.getQuestions();
+          this.getAnswers();
+          this.getAttachments();
+          this.getFeedbacks();
         }
       } catch (error) {
-
+        console.log('Error ' + error.message);
       }
     },
+    async getFeedbacks() {
+      try {
+        const result = await axios.get('http://localhost:8080/feedback', { student: this.exam.studentId });
+        if (result.status == 200) {
+          result.data.forEach((f) => {
+            this.feedbacks[f.question._id] = f.comment;
+          })
+        }
+      } catch (error) {
+        console.log('Error ', error.message);
+      }
+    },
+    async getAttachments() {
+      try {
+        const result = await axios.get('http://localhost:8080/attachment/attachments', { exam: this.$route.params.id });
+        if (result.status == 200) {
+          result.data.forEach((a) => {
+            this.attachments[a.question] = 'http://localhost:8080/' + a.path;
+          })
+        }
+      } catch (error) {
+        console.log('Error : ', error.message);
+      }
+    },
+    async getAnswers() {
+      try {
+        const result = await axios.post('http://localhost:8080/answer/exam/' + this.$route.params.id);
+        if (result.status == 200) {
+          this.answers = result.data
+          this.calculateQuestions();
+        }
+      } catch (error) {
+        console.log('Error : ' + error.message);
+      }
+    }
   },
 };
 </script>
 <template>
   <AuthenticatedLayout>
-    <div class="space-y-12 ml-6 w-1/2">
+    <div class="space-y-12 ml-6 w-full">
       <div class="border-b border-gray-900/10 pb-12">
         <h2 class="text-[32px] font-semibold leading-7 text-gray-900 mb-4 mt-4">
           Survey Details
+          <router-link to="/survey"
+            class="text-xs font-semibold leading-6 text-gray-900 bg-slate-500 px-3 py-2 rounded-sm float-right">
+            Back
+          </router-link>
         </h2>
-        <p class="text-blue-700 font-bold text-[18px]">Total Seen: {{ totalRead }}, Total Attempt : {{ totalAttempt }}</p>
+        <p class="text-blue-700 font-bold text-[18px]">Total Seen: {{ answers.length }}, Total Attempt : {{ totalAttempt
+        }}</p>
         <hr class="mt-6" />
         <Loader v-if="loading" class="mt-10" />
         <div v-else class="container p-4">
@@ -97,27 +131,36 @@ export default {
             <li v-for="(question, index) in questions" :key="index" class="mb-4">
               <div class="flex flex-col">
                 <p class="font-semibold text-[18px]">{{ (parseInt(index) + 1) }}. {{ question.title }}</p>
-                <ul class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400 ml-6">
-                  <li v-for="(option, p) in question.options" :key="parseInt(p) + 2" class="flex items-center">
-                    <svg v-if="question.answer == option._id"
-                      class="w-3.5 h-3.5 me-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                      <path
-                        d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                    </svg>
-                    <svg v-else class="w-3.5 h-3.5 me-2 text-gray-500 dark:text-gray-400 flex-shrink-0" aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                      <path
-                        d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                    </svg>
-                    {{ option.value }}
-                  </li>
-                </ul>
-                <p v-if="getStudentAnswer(question)" :class="`p-2 font-semibold ${ question.answer == getStudentAnswer(question)?._id ? 'text-green-500' : ''}`">Student's Answer : {{ getStudentAnswer(question).value }} </p>
+                <div class="w-1/2">
+                  <ul class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400 ml-6">
+                    <li v-for="(option, p) in question.options" :key="parseInt(p) + 2" class="flex items-center">
+                      <svg v-if="question.answer == option._id"
+                        class="w-3.5 h-3.5 me-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path
+                          d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                      </svg>
+                      <svg v-else class="w-3.5 h-3.5 me-2 text-gray-500 dark:text-gray-400 flex-shrink-0"
+                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path
+                          d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                      </svg>
+                      {{ option.value }}
+                    </li>
+                  </ul>
+                </div>
+                <div class="w-1/2">
+                  <img v-if="attachments[question._id]" :src="attachments[question._id]" class="attachment" alt="attachment">
+                  <img v-if="attachments[question._id]" :src="attachments[question._id]" class="attachment-preview" alt="attachment">
+                </div>
+                <p v-if="getStudentAnswer(question._id)" class="p-2 font-semibold text-green-500">User's Answer : {{
+                  getStudentAnswer(question._id) }} </p>
+                <p v-if="feedbacks[question._id]" class="p-2 font-semibold text-green-500">User's Feedback : {{
+                  feedbacks[question._id] }} </p>
               </div>
             </li>
           </ul>
         </div>
       </div>
-  </div>
+    </div>
 </AuthenticatedLayout></template>
--- src/views/FeedbackDetail.vue
+++ src/views/FeedbackDetail.vue
@@ -19,7 +19,7 @@ export default {
   methods: {
     async getFeedbackDetails() {
       try {
-        const result = await axios.get('http://localhost:8080/feedback/' + this.$route.params.id);
+        const result = await axios.post('http://localhost:8080/feedback/detail/' + this.$route.params.id);
         if (result.status == 200) {
           this.feedback = result.data;
         }
@@ -32,16 +32,36 @@ export default {
 </script>
 <template>
   <AuthenticatedLayout>
-    <div class="space-y-12 ml-6 w-1/2">
+    <div class="space-y-12 ml-6 w-full">
       <div class="border-b border-gray-900/10 pb-12">
         <h2 class="text-[32px] font-semibold leading-7 text-gray-900 mb-4 mt-4">
           Feedback Details
+          <router-link to="/feedback"
+            class="text-xs font-semibold leading-6 text-gray-900 bg-slate-500 px-3 py-2 rounded-sm float-right">
+            Back
+          </router-link>
         </h2>
         <hr class="mt-6" />
         <Loader v-if="loading" class="mt-10" />
-        <div v-else class="container p-4">
-          <p>Question: <b></b></p>
+        <div v-if="feedback" class="relative overflow-x-auto p-4">
+          <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+            <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
+              <tr>
+                <td class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Student</td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">{{ feedback.student.name }}</td>
+              </tr>
+              <tr>
+                <td class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Question</td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">{{ feedback.question.title }}</td>
+              </tr>
+              <tr>
+                <td class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Comment</td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">{{ feedback.comment }}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
-  </div>
-</AuthenticatedLayout></template>
+    </div>
+  </AuthenticatedLayout>
+</template>
